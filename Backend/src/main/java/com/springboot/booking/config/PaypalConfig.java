44ff2563bc9c15c09072bb273bfec2a08d03bbac
Tung Nguyen <tungnguyenn0504@gmail.com>
2024-04-04 17:51:31 +0700

    feature: design paypal service

--- Backend/src/main/java/com/springboot/booking/config/PaypalConfig.java
+++ Backend/src/main/java/com/springboot/booking/config/PaypalConfig.java
@@ -1,11 +1,21 @@
 package com.springboot.booking.config;
 
+import com.paypal.base.rest.APIContext;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PaypalConfig {
+    @Value("${paypal.client-id}")
     private String clientId;
+    @Value("${paypal.secret}")
     private String secret;
+    @Value("${paypal.mode}")
     private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(clientId, secret, mode);
+    }
 }
